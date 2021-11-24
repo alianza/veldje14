699a9089affbbd50d07d0b97177f0d1e1317ad26
alianza <janwillemvanbremen@live.nl>
2021-11-24 18:33:25 +0100

    Added website maker

--- components/sections/Header.js
+++ components/sections/Header.js
@@ -19,13 +19,14 @@ export default function Header({names}) {
             header.classList.remove('h-screen')
             header.classList.add('h-[216px]')
             document.body.classList.remove('overflow-hidden')
+            header.querySelector('span').remove()
         }, 2500)
         return () => clearTimeout(timeout)
     }, [])
 
     return(
         <header
-            className="flex flex-col items-center justify-center w-full p-12 text-center h-screen transition-all duration-1000 ease-in-out overflow-hidden">
+            className="flex flex-col items-center justify-center w-full p-12 text-center h-screen transition-all duration-1000 ease-in-out overflow-hidden relative">
             <h1 className="text-4xl xs:text-6xl font-bold">
                 Welcome to <br/>
                 <a className="text-blue-600" href="https://goo.gl/maps/E1F2CHLvFz5oSwBKA">{name}</a>
