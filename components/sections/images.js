cd177b3add29ef963530512f46c4001f61b585be
alianza <janwillemvanbremen@live.nl>
2021-11-25 14:07:16 +0100

    Restore functionality

--- components/sections/images.js
+++ components/sections/images.js
@@ -6,24 +6,24 @@ export default function Images() {
     useEffect(() => {
         document.querySelectorAll('#images video').forEach(video => { video.playbackRate = .5 })
 
-        // const timeout = setTimeout(() => {
-        //     document.querySelectorAll('#images video').forEach(video => { video.play() })
-        // }, 2500)
-        //
-        // return () => clearTimeout(timeout)
+        const timeout = setTimeout(() => {
+            document.querySelectorAll('#images video').forEach(video => { video.play() })
+        }, 2500)
+
+        return () => clearTimeout(timeout)
     }, [])
 
     return(
         <section id="images" className={`${styles.section} flex justify-between flex-col mobile:flex-row`}>
-            <video className={styles.video} autoPlay muted loop playsInline>
+            <video className={styles.video} muted loop playsInline>
                 <source src="assets/slideshows/jw.webm" type="video/webm" />
                 <source src="assets/slideshows/jw.mp4" type="video/mp4" />
             </video>
-            <video className={styles.video} autoPlay muted loop playsInline>
+            <video className={styles.video} muted loop playsInline>
                 <source src="assets/slideshows/seep.webm" type="video/webm" />
                 <source src="assets/slideshows/seep.mp4" type="video/mp4" />
             </video>
-            <video className={styles.video} autoPlay muted loop playsInline>
+            <video className={styles.video} muted loop playsInline>
                 <source src="assets/slideshows/nelda.webm" type="video/webm" />
                 <source src="assets/slideshows/nelda.mp4" type="video/mp4" />
             </video>
